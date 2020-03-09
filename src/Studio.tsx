a264acbab43c29881accde6dab6370068a4b09da
Elin Alm <elin95alm@gmail.com>
2020-03-09 13:10:18 +0100

    form och studio

--- src/Studio.tsx
+++ src/Studio.tsx
@@ -1,5 +1,5 @@
-import React from 'react'
-
+import React, { CSSProperties } from 'react'
+import Image from "./assets/studio.jpg"
 
 interface Props {
 
@@ -10,10 +10,43 @@ interface State {
 
 }
 export default class Studio extends React.Component<Props, State> {
+    
 
     render(){
         return(
-            <h1>Studio</h1>
+            <div style={studioDiv}>
+                    <img style={studioPic} src={Image}></img>
+
+            <p style={studioText}>Galleri Lunden erbjuder en ljus rymlig studio på Lunden 8.
+            Studion är 100 kvm med tre olika rum som alla har högt i tak. 
+            Ni kan hyra studion för utställningar, events och workshops. 
+            Vi kan erbjuda möbler om så önskas. 
+            </p>
+            <p style={{textAlign: "center", fontSize: "1rem", marginBottom: "4rem"}}>Pris per rum/vecka: 5000 kr + moms </p>
+            </div>
+
+           
         )
     }
+}
+
+const studioPic: CSSProperties = {
+    width: "60rem",
+    height: "30rem",
+    objectFit: "cover",
+    marginTop: "2rem"
+}
+
+const studioDiv: CSSProperties = {
+    display: "flex",
+    justifyContent: "center",
+    flexDirection: "column",
+    alignItems: "center", 
+   
+}
+
+const studioText: CSSProperties = {
+    marginTop: "5rem",
+    width: "50%",
+    fontSize: "1rem",
 }
